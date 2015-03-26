c201a98cfb3481d1e3331711cbd5a24b3fd6ad65
James <james@resin.io>
2015-03-26 14:39:13 +0000

    Add jenkins build step script

---
+++ jenkins_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set env var
+LIBRARY=$1
+# Jenkins build steps
+cd bashbrew/
+
+# Build all node images
+./bashbrew.sh build $LIBRARY --library=../library --namespaces=resin
+
+# Push all images
+./bashbrew.sh push $LIBRARY --library=../library --namespaces=resin
